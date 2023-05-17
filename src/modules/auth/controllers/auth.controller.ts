1a02c35340c5abde84ee40d27a0fd82497b227bc
lazar0169 <lazar0169@gmail.com>
2023-05-17 16:48:15 +0200

    Status codes package changed

--- src/modules/auth/controllers/auth.controller.ts
+++ src/modules/auth/controllers/auth.controller.ts
@@ -1,4 +1,4 @@
-import { HttpStatus } from 'http-status-ts';
+import { StatusCodes } from 'http-status-codes';
 import { Request, Response } from 'express';
 import ReqHandler from '../../../core/request';
 import { AuthDto } from '../dto/auth.dto';
@@ -6,5 +6,5 @@ import { testService } from '../services/auth.service';
 
 export const authRequestHandler = ReqHandler((req: Request, res: Response) => {
     const dataToSend = testService(req.body as unknown as AuthDto);
-    return res.status(HttpStatus.OK).send({ message: 'Everything is ok', user: dataToSend });
+    return res.status(StatusCodes.OK).send({ message: 'Everything is ok', user: dataToSend });
 });
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -1,9 +1,9 @@
+import { StatusCodes } from 'http-status-codes';
 import { AuthDto } from '../dto/auth.dto';
 import log from '../../../utils/logger';
 import hider from '../../../utils/hider';
 import ApiError from '../../../core/error';
 import { ErrorCodes } from '../../../core/errorCodes';
-import { HttpStatus } from 'http-status-ts';
 import { sign } from './cryoto.service';
 
 export const testService = (data?: AuthDto) => {
@@ -12,5 +12,5 @@ export const testService = (data?: AuthDto) => {
         log.info({ email, password: hider(password) }, 'Testing Auth route');
         return sign({ email });
     }
-    throw new ApiError(HttpStatus.UNAUTHORIZED, ErrorCodes.AUTH);
+    throw new ApiError(StatusCodes.UNAUTHORIZED, ErrorCodes.AUTH);
 };
